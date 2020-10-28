45975d18f52519f4d189cd03c24bb44d0b008a71
Sadikchhya Khatiwada <sadikchhya1@gmail.com>
2020-10-28 15:16:00 -0600

    Changes

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b2faac251dc6e7f60522c9fa5692993b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA8iz6u2Rhk5reEMEvJlv9ggE4mopB1b7Y"
